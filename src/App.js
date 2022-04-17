c21422ed6d000a8f7b52e86b2d405a3ddade195b
Bonnev <piggov@gmail.com>
2022-04-17 14:55:21 +0300

    Moved the App logic inside its own component, leaving index.js clean

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import ChartJs from './ChartJs';
+import Timeline from './Timeline';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fs = require('fs');
+		// fs.writeFileSync("C:\\input.txt", "marti karti");
+		const contents = fs.readFileSync("C:\\input.txt").toString();
+		const timelineDate = '2022-04-07';
+		// const nextDate = '2022-04-08';
+
+		const lines = contents.split('\n');
+
+		const matrix = lines.filter(line => line.length).map(line => line.split('\t'));
+
+		let data = [];
+
+		matrix.forEach((current, index) => {
+			if (index-1 < 0) return;
+
+			const prevRow = matrix[index-1];
+			
+			const prevRowTime = prevRow[3];
+			const prevRowParts = prevRowTime.split(':');
+			
+			const currentRowTime = current[3];
+			const currentRowParts = currentRowTime.split(':');
+
+			const prevRowSeconds = +prevRowParts[0] * 3600 + +prevRowParts[1] * 60 + +prevRowParts[2];
+			const currentRowSeconds = +currentRowParts[0] * 3600 + +currentRowParts[1] * 60 + +currentRowParts[2];
+
+			const durationSeconds = currentRowSeconds - prevRowSeconds;
+
+			data.push({
+				label: current[0],
+				process: current[0].substring(current[0].lastIndexOf('\\') + 1),
+				content: current[1].replace(/and \d+ more pages/, '').substring(0, 50),
+				title: current[1].replace(/and \d+ more pages/, ''),
+				number: durationSeconds,
+				start: timelineDate + ' ' + prevRowTime,
+				end: timelineDate + ' ' + currentRowTime
+			});
+		});
+
+		setData(data);
+	}, []);
+	
+	return (<>
+		<ChartJs data={data}></ChartJs>
+		<Timeline data={data}></Timeline>
+	</>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ChartJs from './ChartJs';
 import reportWebVitals from './reportWebVitals';
-import Timeline from './Timeline';
-
-const fs = require('fs');
-// fs.writeFileSync("C:\\input.txt", "marti karti");
-const contents = fs.readFileSync("C:\\input.txt").toString();
-const timelineDate = '2022-04-07';
-// const nextDate = '2022-04-08';
-
-const lines = contents.split('\n');
-
-const matrix = lines.filter(line => line.length).map(line => line.split('\t'));
-
-let data = [];
-
-matrix.forEach((current, index) => {
-	if (index-1 < 0) return;
-
-	const prevRow = matrix[index-1];
-	
-	const prevRowTime = prevRow[3];
-	const prevRowParts = prevRowTime.split(':');
-	
-	const currentRowTime = current[3];
-	const currentRowParts = currentRowTime.split(':');
-
-	const prevRowSeconds = +prevRowParts[0] * 3600 + +prevRowParts[1] * 60 + +prevRowParts[2];
-	const currentRowSeconds = +currentRowParts[0] * 3600 + +currentRowParts[1] * 60 + +currentRowParts[2];
-
-	const durationSeconds = currentRowSeconds - prevRowSeconds;
-
-	data.push({
-		label: current[0],
-		process: current[0].substring(current[0].lastIndexOf('\\') + 1),
-		content: current[1].replace(/and \d+ more pages/, '').substring(0, 50),
-		title: current[1].replace(/and \d+ more pages/, ''),
-		number: durationSeconds,
-		start: timelineDate + ' ' + prevRowTime,
-		end: timelineDate + ' ' + currentRowTime
-	});
-});
+import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ChartJs data={data}></ChartJs>
-		<Timeline data={data}></Timeline>
-		{/* <App /> */}
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
